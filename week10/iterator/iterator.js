ccfffb8f84773ba740f3ff30430e55472633c210
Dierk Koenig <dierk.koenig@canoo.com>
2023-05-03 10:04:17 +0200

    week 10 before Fib iterator

---
+++ week10/iterator/iterator.js
@@ -0,0 +1,56 @@
+/**
+ * @typedef ValueDoneType
+ * @template _T_
+ * @property { _T_ }    value - The value of the item, might be undefined when done is true.
+ * @property { Boolean } done - True if the iterator is done, false otherwise.
+ */
+
+/**
+ * @typedef ValueDoneProducerType
+ * @template _T_
+ * @function
+ * @return { ValueDoneType<_T_> }
+ */
+
+/**
+ * @typedef IteratorType
+ * @template _T_
+ * @property { ValueDoneProducerType<_T_> } next - Returns the next item in the iterator.
+ */
+
+/**
+ * @template _T_
+ * @param { _T_ } startValue
+ * @param { (i:_T_) => Boolean } whileFn
+ * @param { (i:_T_) => _T_ } incrementFn
+ * @return { IteratorType<_T_> }
+ * @constructor
+ */
+const Iterator = (startValue, whileFn, incrementFn) => {
+    const next = () => {
+        const proceed = whileFn(startValue);
+        let   value = undefined;
+        if (proceed) {
+            value = startValue;
+            startValue = incrementFn(startValue);
+        }
+        return { value: value, done: !proceed };
+
+    };
+    return /** @type { IteratorType } */{
+        [Symbol.iterator]: () => ({ next })
+    };
+};
+
+/**
+ * @typedef RangeType
+ * @property { IteratorType<Number> } [Symbol.iterator] - Returns the iterator object.
+ */
+
+/**
+ * @param { Number } n - The number of elements in the range, starting from 0, not reaching n, must not be negative.
+ * @return { RangeType }
+ * @constructor
+ */
+
+const Range = n => /** @type { RangeType } */ Iterator(0, i => i < n, i => i + 1);
--- week10/iterator/iteratorTest.js
+++ week10/iterator/iteratorTest.js
@@ -3,6 +3,28 @@
 ( () => {
     const ok = [];
 
+    ok.push(String([...Iterator(0, i=> i < 5, i=> i + 1) ]) === "0,1,2,3,4");
+    ok.push(String([...Iterator(0, i=> i < 0, i=> i + 1) ]) === "");
+    ok.push(String([...Iterator(0, i=> i < 1, i=> i + 1) ]) === "0");
+
 
     report("iterator", ok);
+}) ();
+( () => {
+    const ok = [];
+
+    ok.push(String([...Range(5)]) === "0,1,2,3,4");
+    ok.push(String([...Range(0)]) === "");
+    ok.push(String([...Range(1)]) === "0");
+
+
+    report("range", ok);
+}) ();
+( () => {
+    const ok = [];
+
+    ok.push(String([...Fib(10)]) === "0,1,1,2,3,5,8");
+
+
+    report("fib", ok);
 }) ();
